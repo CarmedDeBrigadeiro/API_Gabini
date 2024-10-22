eaf9fbb4b8e60195d1f3470183ba4eb4e1720146
jotapelopes <jp.santoslopes28@gmail.com>
2024-10-21 21:56:14 -0300

    feat: Adicionando arquitetura e arquivos de entidade e serviço

---
+++ Core/Services/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Services
+{
+    public class AuthService
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+        public string Register(Usuario usuario)
+        {
+            var usuarioExistente = usuarios.Any(u => u.Username == usuario.Username);
+            if (usuarioExistente)
+                return "Usuário já registrado!";
+
+            usuarios.Add(usuario);
+            return "Usuário registrado com sucesso!";
+        }
+
+        public string Login(Usuario login)
+        {
+            var usuario = usuarios.FirstOrDefault(u => u.Username == login.Username && u.Password == login.Password);
+            if (usuario == null)
+                return "Usuário ou senha inválidos!";
+
+            return "Login realizado com sucesso!";
+        }
+    }
+}
