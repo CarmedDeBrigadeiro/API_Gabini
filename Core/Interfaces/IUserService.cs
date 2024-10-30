d5575fd648e8687e220c14c5311f3dc0970b2140
jotapelopes <jp.santoslopes28@gmail.com>
2024-10-30 20:19:52 -0300

    feat: Gerando arquivos de alteração de dados do perfil

---
+++ Core/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IUserService
+    {
+        Usuario GetUserById(int id);
+        string UpdateProfile(int id, Usuario usuario);
+        string ChangePassword(int id, string newPassword);
+        string DeleteUser(int id);
+    }
+}
---
+++ Core/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Services
+{
+    public class UserService : IUserService
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        public Usuario GetUserById(int id)
+        {
+            return usuarios.FirstOrDefault(u => u.Id == id);
+        }
+
+        public string UpdateProfile(int id, Usuario usuarioAtualizado)
+        {
+            var usuario = usuarios.FirstOrDefault(u => u.Id == id);
+            if (usuario == null)
+                return "Usuário não encontrado!";
+
+            usuario.Nome = usuarioAtualizado.Nome;
+            usuario.Email = usuarioAtualizado.Email;
+            usuario.Telefone = usuarioAtualizado.Telefone;
+
+            return "Perfil atualizado com sucesso!";
+        }
+
+        public string ChangePassword(int id, string newPassword)
+        {
+            var usuario = usuarios.FirstOrDefault(u => u.Id == id);
+            if (usuario == null)
+                return "Usuário não encontrado!";
+
+            usuario.SenhaHash = newPassword;
+            return "Senha alterada com sucesso!";
+        }
+
+        public string DeleteUser(int id)
+        {
+            var usuario = usuarios.FirstOrDefault(u => u.Id == id);
+            if (usuario == null)
+                return "Usuário não encontrado!";
+
+            usuarios.Remove(usuario);
+            return "Usuário excluído com sucesso!";
+        }
+    }
+}
