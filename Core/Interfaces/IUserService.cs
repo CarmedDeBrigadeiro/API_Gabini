69ce475c8ffc69130e01e1bb57fb195852c8f3b4
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2024-10-30 21:04:57 -0300

    Update IUserService.cs

--- Core/Interfaces/IUserService.cs
+++ Core/Interfaces/IUserService.cs
@@ -1,17 +1,13 @@
-﻿using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Core.Interfaces
 {
     public interface IUserService
     {
         Usuario GetUserById(int id);
+
         string UpdateProfile(int id, Usuario usuario);
-        string ChangePassword(int id, string newPassword);
+
         string DeleteUser(int id);
     }
 }
