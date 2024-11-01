a58c281004ec01c1ce720295d021bd937ec40501
Gabriel Dos Santos Figueiredo <gabrieldossantosfigueiredo@gmail.com>
2024-11-01 18:19:14 -0300

    Register fixes

---
+++ Core/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace Core.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task AdicionarUsuarioAsync(Usuario usuario); 
+        Task<Usuario> ObterUsuarioAsync(string username); 
+    }
+}
