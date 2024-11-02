9cd54a110f893ebc0f39d9c4d87a3344933b1049
Gabriel Dos Santos Figueiredo <gabrieldossantosfigueiredo@gmail.com>
2024-11-02 10:17:19 -0300

    Modificações nos arquivos de projeto e adição do diretório Services

---
+++ Ports/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Ports.Services;
+using Core.Entities; 
+
+namespace Ports.Services
+{
+    public interface IAuthService
+    {
+        string Register(Usuario usuario);
+        string Login(LoginRequest loginRequest);
+    }
+}
